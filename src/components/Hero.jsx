3264d712270d7b00c99edb2451e854e3e6ae8c87
KiritoSaga <christian_1997@live.nl>
2023-04-11 20:02:35 +0200

    3D Computer added, loader, color change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
-          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+          <div className="w-5 h-5 rounded-full bg-[#18d4fd]"/>
+          <div className="w-1 sm:h-80 h-40 blue-gradient"></div>
         </div>
 
         <div className="">
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Christian</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#18d4fd]">Christian</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a developer with great customer support! <br className="sm:block hidden" />
             Creative Mind & Huge Personality 
