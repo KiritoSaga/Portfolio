b28ee646f4a8f0fb9ae7068fefc9b8ef0fd56ac0
KiritoSaga <christian_1997@live.nl>
2023-04-14 18:25:34 +0200

    Experiences + logo's

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import ct from "./company/ct.png";
+import anizero from "./company/anizero.png";
+import mediapulse from "./company/mediapulse.png";
+import prodes from "./company/prodes.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  ct,
+  anizero,
+  mediapulse,
+  prodes,
   carrent,
   jobit,
   tripguide,
