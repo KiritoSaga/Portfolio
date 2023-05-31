c1eb232a91cf7edf4c1c5775b593d06721c7f332
KiritoSaga <christian_1997@live.nl>
2023-06-01 00:42:00 +0200

    Clarify dummy data

--- src/constants/index.js
+++ src/constants/index.js
@@ -194,7 +194,7 @@ import {
   
   const projects = [
     {
-      name: "Car Rent (Lorem ipsim)",
+      name: "Car Rent (Dummy Data)",
       description:
         "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
       tags: [
@@ -215,7 +215,7 @@ import {
       source_code_link: "https://github.com/",
     },
     {
-      name: "Job IT  (Lorem ipsim)",
+      name: "Job IT  (Dummy Data)",
       description:
         "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
       tags: [
@@ -236,7 +236,7 @@ import {
       source_code_link: "https://github.com/",
     },
     {
-      name: "Trip Guide  (Lorem ipsim)",
+      name: "Trip Guide  (Dummy Data)",
       description:
         "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
       tags: [
